a0206545baa8685ff9876256eaeea5403bfb7bd2
Zepira <equus_amor@hotmail.com>
2023-11-10 15:25:44 +1100

    made directional blocks waterloggable

--- src/main/java/com/palana/babylonmod/block/custom/ModDirectionalBlock.java
+++ src/main/java/com/palana/babylonmod/block/custom/ModDirectionalBlock.java
@@ -1,33 +1,48 @@
 package com.palana.babylonmod.block.custom;
 
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+
 import org.jetbrains.annotations.Nullable;
 
-public class ModDirectionalBlock extends DirectionalBlock {
+public class ModDirectionalBlock extends DirectionalBlock implements SimpleWaterloggedBlock {
     public static DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     public ModDirectionalBlock(Properties pProperties) {
 
         super(pProperties);
-
+        this.registerDefaultState(this.getStateDefinition().any().setValue(WATERLOGGED, true));
     }
 
     @Nullable
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext placeContext) {
-        System.out.println("HELLOE THIS IS THE DIRECTIONAL BLCOK!" + placeContext.getHorizontalDirection());
-        return this.defaultBlockState().setValue(FACING, placeContext.getHorizontalDirection());
+        // System.out.println("HELLOE THIS IS THE DIRECTIONAL BLCOK!" +
+        // placeContext.getHorizontalDirection());
+        FluidState fluidState = placeContext.getLevel().getFluidState(placeContext.getClickedPos());
+        return this.defaultBlockState().setValue(FACING, placeContext.getHorizontalDirection()).setValue(WATERLOGGED,
+                fluidState.getType() == Fluids.WATER);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(FACING);
+        builder.add(FACING, WATERLOGGED);
     }
 
 }
